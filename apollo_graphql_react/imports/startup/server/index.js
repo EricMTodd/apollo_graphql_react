3bfec4605529bc8ec52a94b04273ef2f061aeaee
-thugnasty- <eterictodd@gmail.com>
2019-04-30 07:50:33 -0600

    end of video #5

--- apollo_graphql_react/imports/startup/server/index.js
+++ apollo_graphql_react/imports/startup/server/index.js
@@ -1,4 +1,23 @@
 import { createApolloServer } from "meteor/apollo";
 import { makeExecutableSchema } from "graphql-tools";
 
-createApolloServer({});
+const typeDefs = `
+type Query {
+  init: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    init() {
+      return "Initializing fragtrak";
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+createApolloServer({ schema });
